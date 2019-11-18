502c242d319c985d8ffaaaa94a7cf8ffdbbab566
Sergey Popov <dev.sergey.popov@gmail.com>
2019-11-18 16:19:25 +0300

    fixed config

--- willbetherebot.py
+++ willbetherebot.py
@@ -1,3 +1,5 @@
+import os
+
 import yaml
 import telebot
 
@@ -22,8 +24,13 @@ from scenario.default.default import Default
 with open("config.yml", 'r') as config_file:
     config = yaml.load(config_file, Loader=yaml.Loader)
 
-bot = telebot.TeleBot(config['bot']['token'])
-context = AlchemyContext(config['db']['connector'])
+if os.environ['MODE'] and os.environ['MODE'] == 'RELEASE':
+    bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
+    context = AlchemyContext(os.environ['DB_CONNECTOR'])
+else:
+    bot = telebot.TeleBot(config['bot']['token'])
+    context = AlchemyContext(config['db']['connector'])
+
 state_repo = AlchemyChatStateRepo(context)
 user_repo = AlchemyUserRepo(context)
 chat_repo = AlchemyChatRepo(context)
